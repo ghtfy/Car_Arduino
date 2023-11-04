5158398eda75c24a2b293dcb5f339346d5fe1519
ghtfy <129151142+ghtfy@users.noreply.github.com>
2023-11-04 20:50:46 +0900

    trafficLights.py

--- trafficLights.py
+++ trafficLights.py
@@ -3,22 +3,37 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)  # 카메라 모듈 사용.
 
+# Set the desired frame width and height
+frame_width = 640
+frame_height = 480
+
+# Set the desired frame rate
+frame_rate = 30
+
+# Set the frame width and height
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+
+# Set the frame rate
+cap.set(cv2.CAP_PROP_FPS, frame_rate)
+
 while True:
     ret, frame = cap.read()  # 카메라 모듈 연속 프레임 읽기
 
+    if not ret:
+        continue
+
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # BGR을 HSV로 변환
 
-    # 넓게 설정한 초록색 범위
-    lower_green = np.array([40, 100, 40])
+    # Fine-tuned color ranges for green, red, and yellow
+    lower_green = np.array([40, 40, 40])
     upper_green = np.array([90, 255, 255])
 
-    # 넓게 설정한 빨강색 범위
     lower_red1 = np.array([0, 100, 100])
     upper_red1 = np.array([10, 255, 255])
     lower_red2 = np.array([160, 100, 100])
     upper_red2 = np.array([180, 255, 255])
 
-    # 넓게 설정한 노란색 범위
     lower_yellow = np.array([20, 100, 100])
     upper_yellow = np.array([30, 255, 255])
 
@@ -26,19 +41,19 @@ while True:
     mask_green = cv2.inRange(hsv, lower_green, upper_green)
     mask_red1 = cv2.inRange(hsv, lower_red1, upper_red1)
     mask_red2 = cv2.inRange(hsv, lower_red2, upper_red2)
-    mask_red = mask_red1 + mask_red2  # 빨간색 범위 합치기
+    mask_red = mask_red1 + mask_red2  # Combine the red range
     mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
-    # Bitwise-AND mask and original image
+    # Bitwise-AND mask and the original image
     res_green = cv2.bitwise_and(frame, frame, mask=mask_green)
     res_red = cv2.bitwise_and(frame, frame, mask=mask_red)
     res_yellow = cv2.bitwise_and(frame, frame, mask=mask_yellow)
 
-    # 빨간색, 초록색, 노란색 표시
+    # Display colors: red, green, yellow
     result = cv2.addWeighted(res_red, 1, res_green, 1, 0)
     result = cv2.addWeighted(result, 1, res_yellow, 1, 0)
 
-    # 색상 감지 후 메시지 표시
+    # Display messages based on color detection
     font = cv2.FONT_HERSHEY_SIMPLEX
     if np.sum(mask_red) > 10000:
         cv2.putText(result, 'STOP', (10, 50), font, 2, (0, 0, 255), 2, cv2.LINE_AA)
@@ -47,8 +62,8 @@ while True:
     elif np.sum(mask_green) > 10000:
         cv2.putText(result, 'GO', (10, 50), font, 2, (0, 255, 0), 2, cv2.LINE_AA)
 
-    cv2.imshow('frame', frame)  # 원본 영상을 보여줌
-    cv2.imshow('Traffic Lights', result)  # 빨간색, 초록색, 노란색 및 메시지를 표시
+    cv2.imshow('frame', frame)  # Display the original frame
+    cv2.imshow('Traffic Lights', result)  # Display red, green, yellow, and messages
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
@@ -56,4 +71,5 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
 
